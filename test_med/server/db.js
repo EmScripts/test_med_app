20033066af3ffdaf0ae9d057db2b90cc683b0c43
emmma-mj <emmamariejeanne@gmail.com>
2024-11-02 18:46:36 +0400

    db

--- test_med/server/db.js
+++ test_med/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:L2yAfP3hfmcLdNoyD7A0lJw0@172.21.87.69:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
